75c779ed26a1c70e0b6ec0ae05b79caebfac21de
RookieJoel <jojackchanah@gmail.com>
2025-05-20 09:28:46 +0700

    basic-oprations

---
+++ Operation/op.ts
@@ -0,0 +1,27 @@
+let x = 5+4;
+let y = 5-4;
+let z = 5*4;
+let a = 5/4;
+let b = 5%4;
+let c = 5**4;
+
+x++;
+y--;
+let d = x + y;
+let e = x - y;
+
+//comparison
+let f = 10 == "10";
+console.log(f); //true  
+let f1 = 10 === "10"; 
+console.log(f1); //false
+let g = x != y;
+console.log(g); //true
+let h = x === y;
+console.log(h); //false
+
+//logical
+let i = true && false;
+console.log(i); //false
+let j = true || false;
+console.log(j); //true
