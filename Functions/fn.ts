f591f86d23480e729558b685846671ecd32e5522
RookieJoel <jojackchanah@gmail.com>
2025-08-06 13:18:16 +0700

    test mul func

--- Functions/fn.ts
+++ Functions/fn.ts
@@ -45,3 +45,6 @@ console.log(sum2(1, 2, 3, 4, 5)); // Output: 15
 function multiply(a: number, b: number): number {
     return a * b;
 }
+
+//test multiply function
+console.log(multiply(5, 10)); // Output: 50
